c81bf14cf23ae6e3411cd127dd37d7a0e51ce0b7
Luke Memet <lukememet@gmail.com>
2026-05-01 23:19:30 -0400

    feat(apple-spm): Add Swift Package manifests
    Adds Apple Swift Package Manager manifests and source roots so TimeFinder can consume appsflyer_sdk without CocoaPods.

---
+++ ios/appsflyer_sdk/Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "appsflyer_sdk",
+    platforms: [
+        .iOS("12.0")
+    ],
+    products: [
+        .library(name: "appsflyer-sdk", targets: ["appsflyer_sdk"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework-Static.git", exact: "6.18.0")
+    ],
+    targets: [
+        .target(
+            name: "appsflyer_sdk",
+            dependencies: [
+                .product(name: "AppsFlyerLib-Static", package: "AppsFlyerFramework-Static")
+            ],
+            publicHeadersPath: "."
+        )
+    ]
+)
